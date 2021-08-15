db5c936f767f3e843e0ef810434cfe567f205cae
krishcoderGITHUB <75573226+krishcoderGITHUB@users.noreply.github.com>
2021-08-15 09:23:00 +0530

    Update script.js

--- independenceday/script.js
+++ independenceday/script.js
@@ -18,7 +18,8 @@ $(".LoadText").css("margin-top","300px");
       
 $("body").css("background","url('http://webneel.com/wallpaper/sites/default/files/images/08-2013/18-india-independence-day-wallpaper.preview.jpg')").delay(1000);
 $("body").css("background-size","cover");
-   }
+ $("#promotion").css("display","absolute");  
+}
 
 }
 f();
